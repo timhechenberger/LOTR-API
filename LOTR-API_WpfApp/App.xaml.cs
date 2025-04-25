3288406a6f605389c141477ae3fa3a853075c22e
Tim Hechenberger <timhec44@gmail.com>
2025-04-25 12:04:03 +0200

    Initial commit

---
+++ LOTR-API_WpfApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LOTR_API_WpfApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
